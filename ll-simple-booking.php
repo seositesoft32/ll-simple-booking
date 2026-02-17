5bec7c952411f4dbb7ffb18718f1bb941cf6fd7b
M Bilal Arshad <seositesoft32@gmail.com>
2026-02-17 20:53:13 +0500

    last changes

--- ll-simple-booking.php
+++ ll-simple-booking.php
@@ -2,9 +2,10 @@
 /**
  * Plugin Name: LL Simple Booking Appointments
  * Description: AJAX-based multi-step appointment booking with custom post type, admin table, and availability settings.
- * Version: 1.4
+ * Version: 1.5
  * Author: Lievelingslinnen
  * Requires at least: 6.0
+ * Tested up to: 6.7
  * Requires PHP: 7.4
  * WC requires at least: 8.0
  * WC tested up to: 10.0
@@ -16,7 +17,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('LLSBA_VERSION', '1.4'); 
+define('LLSBA_VERSION', '1.5'); 
 define('LLSBA_FILE', __FILE__);
 define('LLSBA_PATH', plugin_dir_path(__FILE__));
 define('LLSBA_URL', plugin_dir_url(__FILE__));
